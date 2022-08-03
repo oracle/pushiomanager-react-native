1716bac60a5ed33dd79b040260c78477b629771f
Juliovaz <julio.oliveiravaz0@gmail.com>
2022-08-03 16:24:31 -0300

    feat: add debug message

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-rm -rf ./framework/PushIOManager.framework && cp -Rf { echo $PUSH_IO_MANAGER_PATH_IOS }/framework/PushIOManager.framework ./framework/ || { echo "\n\n\n Error ==> PushIOManager.framework not found. Please copy the PushIOManager.framework to YOUR_APP_DIR/ios/framework/ and install package again. Follow README.md Installation instructions.<===  \n\n\n;"
+rm -rf ./framework/PushIOManager.framework && cp -Rf { echo $PUSH_IO_MANAGER_PATH_IOS"/framework/PushIOManager.framework" } ./framework/ || { echo "\n\n\n Error ==> PushIOManager.framework not found. Please copy the PushIOManager.framework to YOUR_APP_DIR/ios/framework/ and install package again. Follow README.md Installation instructions.<===  \n\n\n;" $PUSH_IO_MANAGER_PATH_IOS"/framework/PushIOManager.framework"
  exit 1; }
  fi
